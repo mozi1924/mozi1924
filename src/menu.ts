e8e34b1e10fbca712b6a01c35873a1ce40c3b49d
mozi1924 <15985142983@163.com>
2026-01-26 20:56:39 +0800

    refactor: centralize navigation and footer links into a new `menu.ts` file for dynamic rendering.

---
+++ src/menu.ts
@@ -0,0 +1,24 @@
+export interface NavLink {
+    name: string;
+    href: string;
+}
+
+export interface FooterLink {
+    name: string;
+    href: string;
+}
+
+export const NAV_LINKS: NavLink[] = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Blogs", href: "/blogs" },
+    { name: "Mozi's Rig", href: "/mozi-rig" },
+    { name: "Text2Arch Tool", href: "/text-to-archbase4-encryption-decryption" },
+];
+
+export const FOOTER_LINKS: FooterLink[] = [
+    { name: "GitHub", href: "https://github.com/mozi1924" },
+    { name: "Mozi Store", href: "https://store.mozi1924.com/" },
+    { name: "YouTube", href: "https://www.youtube.com/@moziarasaka" },
+    { name: "Bilibili", href: "https://space.bilibili.com/434156493" },
+];
